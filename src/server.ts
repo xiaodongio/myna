1ae02e0f41fcc1f79d6631a05316abbf3c8a7d4d
xiaodongio <qq282162962@gmail.com>
2019-07-06 14:45:52 +0800

    add socket.io

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import App from "./app";
+import SocketServer from "./socket/SocketServer";
 
-App.initializeApp().then((app) => {
+App.initializeApp().then((server) => {
+    new SocketServer(server);
     console.log(("  App is running at %d in %s mode"), App.app.get("port"), App.app.get("env"));
 });
+
